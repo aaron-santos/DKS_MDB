554f5a3fcdf7141d01725e131e1eab182a735ad7
aaron-santos <aaron-kevin.santos@stud.fh-campuswien.ac.at>
2023-03-02 13:22:22 +0100

    jh

--- src/main/java/at/ac/fhcampuswien/fhmdb/HomeController.java
+++ src/main/java/at/ac/fhcampuswien/fhmdb/HomeController.java
@@ -52,7 +52,6 @@ public class HomeController implements Initializable {
 
         // TODO add event handlers to buttons and call the regarding methods
         // either set event handlers in the fxml file (onAction) or add them here
-
         // Sort button example:
         sortBtn.setOnAction(actionEvent -> {
             if(sortBtn.getText().equals("Sort (asc)")) {
