64047b8c0ccdc16f034615e380dd7584160c8b27
aaron-santos <aaron-kevin.santos@stud.fh-campuswien.ac.at>
2023-03-11 15:45:18 +0100

    test for initializeMovies done

--- src/main/test/at/ac/fhcampuswien/fhmdb/models/MovieTest.java
+++ src/main/test/at/ac/fhcampuswien/fhmdb/models/MovieTest.java
@@ -1,99 +1,114 @@
 package at.ac.fhcampuswien.fhmdb.models;
 
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import static at.ac.fhcampuswien.fhmdb.models.Movie.filterMoviesByNameAndAscending;
 import static at.ac.fhcampuswien.fhmdb.models.Movie.initializeMovies;
+import static at.ac.fhcampuswien.fhmdb.models.Movie.movies;
 import static org.junit.jupiter.api.Assertions.*;
 
-public class MovieTest {
+class MovieTest {
 
     @Test
-    void checks_if_list_of_initialized_movies_is_not_empty() {
-        List<Movie> movies = initializeMovies();
+    void checks_if_initialized_movies_is_not_empty(){
+        //given
+        movies.clear();
+        initializeMovies();
+        //when
+        boolean actual = movies.isEmpty();
+        //then
+        assertFalse(actual);
 
-        // Test that the list of movies is not empty
-        assertFalse(movies.isEmpty());
-    }
-
-    @Test
-    void lists_movies_that_contains_search_term_in_title() {
-        List<Movie> movies = initializeMovies();
-
-        // Test that the list of movies contains at least one movie with the title "The Dog"
-        boolean hasTheDog = false;
-        for (Movie movie : movies) {
-            if (movie.getTitle().equals("The Dog")) {
-                hasTheDog = true;
-                break;
-            }
-        }
-        assertTrue(hasTheDog);
     }
     @Test
-    public void sort_movies_in_ascending_order_with_search_term() {
-        // Arrange
-        String searchTerm = "the";
-        boolean sortAsc = true;
-        List<Movie> movies = initializeMovies();
-        int expectedSize = 4;
-        String[] expectedTitles = {"The Dog", "The Incredibles", "The Title1", "The Title2"};
-
-        // Act
-        List<Movie> filteredMovies = filterMoviesByNameAndAscending(movies, searchTerm, sortAsc);
-
-        // Assert
-        for (int i = 0; i < expectedSize; i++) {
-            assertEquals(expectedTitles[i], filteredMovies.get(i).getTitle());
-        }
+    void checks_if_all_4_dummy_movies_are_initialized(){
+        movies.clear();
+        initializeMovies();
+        assertEquals(4, movies.size());
     }
     @Test
-    public void sort_movies_in_ascending_order_without_search_term() {
-        // Arrange
-        String searchTerm = "";
-        boolean sortAsc = true;
-        List<Movie> movies = initializeMovies();
-        int expectedSize = 6;
-        String[] expectedTitles = {"CatsVenture", "Generic Movie Title", "The Dog", "The Incredibles", "The Title1", "The Title2"};
-
-        // Act
-        List<Movie> filteredMovies = filterMoviesByNameAndAscending(movies, searchTerm, sortAsc);
-
-        // Assert
-        for (int i = 0; i < expectedSize; i++) {
-            assertEquals(expectedTitles[i], filteredMovies.get(i).getTitle());
-        }
+    void checks_if_size_of_movies_list_is_not_greater_than_4(){
+        movies.clear();
+        initializeMovies();
+        assertNotEquals(5,movies.size());
     }
     @Test
-    public void number_of_movies_sorted_ascending_with_search_term() {
-        // Arrange
-        String searchTerm = "the";
-        boolean sortAsc = true;
-        List<Movie> movies = initializeMovies();
-        int expectedSize = 4;
-        String[] expectedTitles = {"The Dog", "The Incredibles", "The Title1", "The Title2"};
+    void first_added_movie_title_is_correct(){
+        movies.clear();
+        movies.add(new Movie("The Incredibles", "This is a dummy description",
+                Arrays.asList(Genre.COMEDY, Genre.ADVENTURE, Genre.ANIMATION, Genre.ACTION)));
+        initializeMovies();
 
-        // Act
-        List<Movie> filteredMovies = filterMoviesByNameAndAscending(movies, searchTerm, sortAsc);
+        String actualTitle = movies.get(0).getTitle();
 
-        // Assert
-        assertEquals(expectedSize, filteredMovies.size());
+        String expectedTitle = "The Incredibles";
+        assertEquals(expectedTitle, actualTitle);
     }
     @Test
-    public void number_of_movies_sorted_ascending_without_search_term() {
-        // Arrange
-        String searchTerm = "";
-        boolean sortAsc = true;
-        List<Movie> movies = initializeMovies();
-        int expectedSize = 6;
+    void first_added_movie_description_is_correct(){
+        movies.clear();
+        movies.add(new Movie("Minions", "Minions dummy description",
+                Arrays.asList(Genre.ANIMATION, Genre.COMEDY,Genre.ACTION, Genre.ADVENTURE)));
+        initializeMovies();
 
-        // Act
-        List<Movie> filteredMovies = filterMoviesByNameAndAscending(movies, searchTerm, sortAsc);
+        String actualDescription = movies.get(0).getDescription();
 
-        // Assert
-        assertEquals(expectedSize, filteredMovies.size());
+        String expectedDescription = "Minions dummy description";
+        assertEquals(expectedDescription, actualDescription);
     }
+    @Test
+    void first_added_movie_genres_are_correct(){
+        movies.clear();
+        movies.add(new Movie("The Lion King", "The Lion King dummy description",
+                Arrays.asList(Genre.ANIMATION, Genre.DRAMA, Genre.ADVENTURE)));
+        initializeMovies();
+
+        List<Genre> actualGenres = movies.get(0).getGenres();
+
+        List<Genre> expectedGenres = Arrays.asList(Genre.ANIMATION, Genre.DRAMA, Genre.ADVENTURE);
+        assertEquals(expectedGenres, actualGenres);
+    }
+    @Test
+    void added_movie_title_is_correct(){
+        movies.clear();
+        initializeMovies();
+        movies.add(new Movie("The Incredibles", "This is a dummy description",
+                Arrays.asList(Genre.COMEDY, Genre.ADVENTURE, Genre.ANIMATION, Genre.ACTION)));
+
+        String actualTitle = movies.get(4).getTitle();
+
+        String expectedTitle = "The Incredibles";
+        assertEquals(expectedTitle, actualTitle);
+    }
+    @Test
+    void added_movie_description_is_correct(){
+        movies.clear();
+        initializeMovies();
+        movies.add(new Movie("Minions", "Minions dummy description",
+                Arrays.asList(Genre.ANIMATION, Genre.COMEDY,Genre.ACTION, Genre.ADVENTURE)));
+
+        String actualDescription = movies.get(4).getDescription();
+
+        String expectedDescription = "Minions dummy description";
+        assertEquals(expectedDescription, actualDescription);
+    }
+    @Test
+    void added_movie_genres_are_correct(){
+        movies.clear();
+        initializeMovies();
+        movies.add(new Movie("The Lion King", "The Lion King dummy description",
+                Arrays.asList(Genre.ANIMATION, Genre.DRAMA, Genre.ADVENTURE)));
+
+        List<Genre> actualGenres = movies.get(4).getGenres();
+
+        List<Genre> expectedGenres = Arrays.asList(Genre.ANIMATION, Genre.DRAMA, Genre.ADVENTURE);
+        assertEquals(expectedGenres, actualGenres);
+    }
+
+
+
 
 }
