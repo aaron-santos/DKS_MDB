91fc3e72f35f0cdc00e9934a7cbf1ed192d3d953
aaron-santos <aaron-kevin.santos@stud.fh-campuswien.ac.at>
2023-03-11 13:37:05 +0100

    changed javafx version and dummy movies

--- src/main/java/at/ac/fhcampuswien/fhmdb/models/Movie.java
+++ src/main/java/at/ac/fhcampuswien/fhmdb/models/Movie.java
@@ -41,24 +41,33 @@ public class Movie {
 
     public static void initializeMovies(){
         movies.add(new Movie(
-                "The Dog",
-                "Movie about a cute dog.Movie about a cute dog.Movie about a cute dog.Movie about a cute dog.Movie about a cute dog.Movie about a cute dog.Movie about a cute dog.Movie about a cute dog.Movie about a cute dog.",
-                Arrays.asList(Genre.ANIMATION, Genre.FAMILY)
+                "Avengers: End Game",
+                "Nebula and Tony Stark are stranded in space following their defeat by Thanos on Titan, " +
+                        "but are returned to Earth by Carol Danvers and reunited with Natasha Romanoff, Bruce Banner, " +
+                        "Steve Rogers, Rocket, Thor, and James Rhodes.",
+                Arrays.asList(Genre.ACTION, Genre.ADVENTURE, Genre.FANTASY, Genre.SCIENCE_FICTION)
         ));
         movies.add(new Movie(
-                "CatsVenture",
-                "A cute cat on an adventure.",
-                Arrays.asList(Genre.ANIMATION, Genre.ADVENTURE)
+                "Free Guy",
+                "It tells the story of a bank teller who discovers that he is a non-player character in a " +
+                        "massively multiplayer online game who then partners with a player to find evidence that a " +
+                        "gaming company's CEO stole the player's game's source code.",
+                Arrays.asList(Genre.COMEDY, Genre.ACTION, Genre.SCIENCE_FICTION, Genre.ADVENTURE)
         ));
         movies.add(new Movie(
-                "Generic Movie Title",
-                "Generic movie description.",
-                Arrays.asList(Genre.MYSTERY)
+                "Pulp Fiction",
+                "Pulp fiction refers to a genre of racy, action-based stories published in cheaply printed " +
+                        "magazines from around 1900 to the 1950s, mostly in the United States. Pulp fiction gets its " +
+                        "name from the paper it was printed on.",
+                Arrays.asList(Genre.COMEDY, Genre.THRILLER, Genre.CRIME, Genre.DRAMA)
         ));
         movies.add(new Movie(
-                "The Title",
-                "The description.",
-                Arrays.asList(Genre.COMEDY)
+                "The Hunger Games",
+                "Synopsis. The nation of Panem is divided into 12 districts, ruled from the Capitol. As " +
+                        "punishment for a failed revolt, each district is forced to select two tributes, one boy and " +
+                        "one girl between the ages of 12 and 18, to fight to the death in the annual Hunger Games until " +
+                        "there is only one survivor.",
+                Arrays.asList(Genre.SCIENCE_FICTION, Genre.THRILLER, Genre.ADVENTURE)
         ));
     }
 
